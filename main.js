86a68dd5bccf83747cc34cdc2bdb34172d8b622d
billyGit9102 <billy.git9102@gmail.com>
2019-09-08 14:02:33 +0800

    ipcMainHanlder js coding

--- main.js
+++ main.js
@@ -4,20 +4,20 @@
 *  - recieve expand to control size
 * ========================================== */
 
-const {app, ipcMain} = require('electron');
+const {app} = require('electron');
 const  {createWindow}=require('./src/browserWindow/mainWindow');
-const {set_sizeExpand, set_sizeMini} = require('./src/browserWindow/winResizefunc');
+
+const  {ipcMainHandlerInit}=require('./src/ipcMainHandler');
+
 
 let mainWindow;
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', () => setTimeout(()=>{
   console.log("createWindow");
   mainWindow=createWindow();
-  //console.log(mainWindow);
+  ipcMainHandlerInit(mainWindow);
 }, 3000));
+
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -27,31 +27,3 @@ app.on('window-all-closed', function () {
     app.quit();
   }
 })
-
-
-/* ---------------------------------------------------- *
-*  ■ recieve expand to control size
-* ----------------------------------------------------- */
-let expand=true;
-ipcMain.on('timer:expand', function(e, expand){
-  if(expand){
-    set_sizeExpand(mainWindow);
-  }else{
-    set_sizeMini(mainWindow);
-  }
-  console.log("timer:expand",expand);
-});
-ipcMain.on('appLoadDone', function(e){
-  console.log("appLoadDone");
-  
-  //console.log(mainWindow);
-
-  if(expand){
-    set_sizeExpand(mainWindow);
-  }else{
-    set_sizeMini(mainWindow);
-  }
-
-});
-
-/*---end recieve expand to control size ---*/
---
+++ src/ipcMainHandler.js
@@ -0,0 +1,37 @@
+
+const {ipcMain} = require('electron');
+const {set_sizeExpand, set_sizeMini} = require('./browserWindow/winResizefunc');
+
+/* ---------------------------------------------------- *
+*  ■ recieve expand to control size
+* ----------------------------------------------------- */
+function ipcMainHandlerInit(mainWindow){
+
+    let expand=true;
+    
+    ipcMain.on('timer:expand', function(e, expand){
+        if(expand){
+            set_sizeExpand(mainWindow);
+        }else{
+            set_sizeMini(mainWindow);
+        }
+        console.log("timer:expand",expand);
+    });
+    
+    ipcMain.on('appLoadDone', function(e){
+    console.log("appLoadDone");
+    
+    //console.log(mainWindow);
+
+        if(expand){
+            set_sizeExpand(mainWindow);
+        }else{
+            set_sizeMini(mainWindow);
+        }
+
+    });
+
+}
+module.exports = {
+  ipcMainHandlerInit
+}
