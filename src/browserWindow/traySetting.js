37921431cf564d82d5398cc7ce9d6bbe5632379f
billyGit9102 <billy.git9102@gmail.com>
2019-09-08 12:22:54 +0800

    file structure clearing done

--- src/browserWindow/traySetting.js
+++ src/browserWindow/traySetting.js
@@ -1,48 +1,48 @@
 const {Tray, Menu} = require('electron');
 const path=require('path');
-const iconPath = path.join(__dirname, '../icon/img-300x300.jpg');
+const iconPath = path.join(__dirname, '../../icon/img-300x300.jpg');
 let tray=null;
 
 function traySetting(){
 
     tray=new Tray(iconPath);
-   // tray.on('double-click',function(){
-        //mainWindow.show();
-   //  })
-    // let template = [
-    //     {
-    //         label: 'Audio',
-    //         submenu:[
-    //             {
-    //                 label:'Low',
-    //                 type:'radio',
-    //                 checked: true
-    //             },
-    //             {
-    //                 label:'High',
-    //                 type:'radio'
-    //             }
-    //         ]
-    //     },
-    //     {
-    //         label: 'Video',
-    //         submenu:[
-    //             {
-    //                 label:'Low',
-    //                 type:'radio',
-    //                 checked: true
-    //             },
-    //             {
-    //                 label:'High',
-    //                 type:'radio'
-    //             }
-    //         ]
-    //     }
-    // ]
-    // const ctxMenu = Menu.buildFromTemplate(template);
+    tray.on('double-click',function(){
+        mainWindow.show();
+    })
+    let template = [
+        {
+            label: 'Audio',
+            submenu:[
+                {
+                    label:'Low',
+                    type:'radio',
+                    checked: true
+                },
+                {
+                    label:'High',
+                    type:'radio'
+                }
+            ]
+        },
+        {
+            label: 'Video',
+            submenu:[
+                {
+                    label:'Low',
+                    type:'radio',
+                    checked: true
+                },
+                {
+                    label:'High',
+                    type:'radio'
+                }
+            ]
+        }
+    ]
+    const ctxMenu = Menu.buildFromTemplate(template);
 
-    // tray.setContextMenu(ctxMenu);
-    // tray.setToolTip('Doing Timer');
+    tray.setContextMenu(ctxMenu);
+    tray.setToolTip('Doing Timer');
 }
 
 module.exports = {
