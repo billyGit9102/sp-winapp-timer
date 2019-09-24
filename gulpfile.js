9adff0802ecf49a7959a32f7f9eed1ed776bd6f8
billyGit9102 <billy.git9102@gmail.com>
2019-09-24 23:11:01 +0800

    gulp modify, css added

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-const { src, dest,watch } = require('gulp');
-const fileinclude = require('gulp-file-include');
-const sass = require('gulp-sass');
-const autoprefixer = require("autoprefixer");
-const postcss = require("gulp-postcss");
-=======
 const {watchJs}= require('./gulp/watchJs');
 const {watchHtml}= require('./gulp/watchHtml');
 const {watchScss}= require('./gulp/watchScss');
->>>>>>> 86529c1fa84108425bd75030372193ec7e5692b9
 
 function watchSrc() {
     console.log("watching src");
