f68aad9f45a983915ad5cd7ac0524bb494901eed
billyGit9102-sg <billy.git9102@gmail.com>
2019-09-26 14:25:46 +0800

    gulp html minifie added

--- gulp/watchHtml.js
+++ gulp/watchHtml.js
@@ -1,5 +1,6 @@
 const { src, dest,watch } = require('gulp');
 const fileinclude = require('gulp-file-include');
+const htmlmin = require('gulp-htmlmin');
 
 function compileHtml() {
     // place code for your default task here
@@ -9,6 +10,7 @@ function compileHtml() {
       prefix: '@@',
       basepath: '@file'
     }))
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(dest('./'));
 }
 function watchHtml(){
