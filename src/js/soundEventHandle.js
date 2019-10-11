fc7cf2f0b182a3ecd259ba674cc15be2c504b40c
billyGit9102-sg <billy.git9102@gmail.com>
2019-10-11 13:57:43 +0800

    soundEventHandle arrow func

--- src/js/soundEventHandle.js
+++ src/js/soundEventHandle.js
@@ -2,13 +2,13 @@ import {soundProcess,sound1Min,sound2Min,sound5Min} from './sound';
 import  { base_url,type } from './globalVar_html.js';
 
 const soundEventHandleInit=(timerControl)=>{
-    document.addEventListener("sound:start", function() {
+    document.addEventListener("sound:start", ()=>{
         console.log("sound:start");
         soundProcess.currentTime = 0;
         soundProcess.play();
     });
 
-    document.addEventListener("sound:process", function() {
+    document.addEventListener("sound:process", ()=>{
         $.post(base_url+'doing_timer/set_ticks/'+type, {
             "ticks": timerControl.getTicks(),
             "endTime": $("#endTime").val()
@@ -20,22 +20,22 @@ const soundEventHandleInit=(timerControl)=>{
         soundProcess.play();
     });
 
-    document.addEventListener("sound:1min", function() {
+    document.addEventListener("sound:1min", ()=>{
         console.log("sound:1min");
         sound1Min.play();
     });
 
-    document.addEventListener("sound:2min", function() {
+    document.addEventListener("sound:2min", ()=>{
         console.log("sound:2min");
         sound2Min.play();
     });
 
-    document.addEventListener("sound:5min", function() {
+    document.addEventListener("sound:5min", ()=>{
         console.log("sound:5min");
         sound5Min.play();
     });
 
-    document.addEventListener("sound:End", function() {
+    document.addEventListener("sound:End", ()=>{
         console.log("sound:End");
         $("body").addClass("timerAlert");
         $("#expander").trigger('click');
