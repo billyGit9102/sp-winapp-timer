6bc676ae3035052a4b02a5589c41fcd575d560a8
billyGit9102 <billy.git9102@gmail.com>
2019-10-12 23:50:26 +0800

    change to self invoke arrow function

--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,7 @@ import {soundEventHandleInit} from './soundEventHandle';
 //const electron = window.require('electron');
 //const {ipcRenderer} = electron;
 
-(function () {
+(()=>{
     showPreloader()
     ipcRendererInit();
    
@@ -38,4 +38,4 @@ import {soundEventHandleInit} from './soundEventHandle';
     .catch((error)=>{
           console.log('There has been a problem with your fetch operation: ', error.message);
     });
-}()); //--end (function ($) {
+})(); //--end (function ($) {
