27f74bb20633061661a0e3a71cfdb31d301c19e8
billyGit9102 <billy.git9102@gmail.com>
2019-10-22 23:40:06 +0800

    resize code redo

--- src/browserWindow/resize/winResizefunc.js
+++ src/browserWindow/resize/winResizefunc.js
@@ -9,23 +9,50 @@ let set_reizeMainWindowVar=(mW)=>{
 }
 let saveSizeWrap=()=>{
     saveSize(mainWindow);
+    console.log('max',mainWindow.getSize())
 }
-let set_sizeExpand=(mainWindow)=>{
-    //console.log("set_sizeExpand");
-    mainWindow.setResizable(true);
-    mainWindow.setSize(winW(),winH());
-    //prevent multiple saveSize
-    mainWindow.removeListener('resize', saveSizeWrap);
-    mainWindow.addListener('resize', saveSizeWrap);
+const saveMinSize=(e)=>{
+    let size=mainWindow.getSize();
+    console.log('min',size)
 }
-let set_sizeMini=(mainWindow)=>{
-    //console.log("set_sizeMini");
-    mainWindow.removeListener('resize', saveSizeWrap);
-    mainWindow.setResizable(true);
-    mainWindow.setSize(winW_mini,winH_mini);
-    mainWindow.setResizable(false);
-}
+// let set_sizeExpand=(mainWindow)=>{
+//     mainWindow.setMaximizable(true)
+//     mainWindow.setMinimumSize(200, 400);
+//     mainWindow.setMaximumSize(3000, 3000);
+
+//     console.log("set_sizeExpand");
+//     console.log(winW(),winH());
+//     mainWindow.setResizable(true);
+//     mainWindow.setSize(winW(),winH());
+
+      
+//     mainWindow.removeListener('resize', saveMinSize);
+//     //prevent multiple saveSize
+//     mainWindow.removeListener('resize', saveSizeWrap);
+//     mainWindow.addListener('resize', saveSizeWrap);
+// }
+// let set_sizeMini=(mainWindow)=>{
+//     //can't maximize in min mode
+//     mainWindow.setMaximizable(false)
+//     //restrict size
+//     mainWindow.setMinimumSize(150, 60);
+//     mainWindow.setMaximumSize(800, 60)
+
+//     //console.log("set_sizeMini");
+//     mainWindow.setResizable(true);
+//     mainWindow.setSize(winW_mini,winH_mini);
+
+//     //remove big size save listener
+//     mainWindow.removeListener('resize', saveSizeWrap);
+//     //
+//     mainWindow.removeListener('resize', saveMinSize);    
+//     mainWindow.addListener('resize', saveMinSize);
+
+//     //mainWindow.setResizable(false);
+//     //mainWindow.setBounds({ width: 800, height: 60 })
+// }
 
 module.exports = {
-    set_sizeExpand,set_sizeMini,set_reizeMainWindowVar
+    //set_sizeExpand,set_sizeMini,
+    set_reizeMainWindowVar
 }
--- src/browserWindow/saveLoadSize.js
+++ src/browserWindow/saveLoadSize.js
@@ -6,14 +6,15 @@ const minExpandWidth=200;
 const minExpandHeight=350;
 
 let saveSize=(e)=>{
-    //console.log("window resize");
+    console.log("window resize");
     //console.log(e.sender.getSize());
     let winSize = { 
         width: e.getSize()[0],
         height: e.getSize()[1]
     };
     let data = JSON.stringify(winSize, null, 2);
-
+    
+    console.log(data);
     fs.writeFile(dataSizePath, data, (err) => {
         if (err) throw err;
         //console.log('Data written to file');
@@ -22,15 +23,19 @@ let saveSize=(e)=>{
 let loadSize=()=>{    
     let size;
     if (fs.existsSync(dataSizePath)){ 
+        console.log('fileexist')
         let rawdata = fs.readFileSync(dataSizePath);
         size = JSON.parse(rawdata);    
+        console.log(size)
     }else{
+        console.log('fil not exist')
         size={};
         size.width=600;        
         size.height=600;
     }
-    size.width=size.width<minExpandWidth?minExpandWidth:size.width;
-    size.height=size.height<minExpandHeight?minExpandHeight:size.height;
+
+    //size.width=size.width<minExpandWidth?minExpandWidth:size.width;
+    //size.height=size.height<minExpandHeight?minExpandHeight:size.height;
     return size;
 }
 
